8e7a05f78608a0d9e95c1808f82b76691a91a6cf
Jelly Bean <jelly_b@sina.com>
2021-10-02 00:11:30 +0800

    Move a interface method from IBinaryMessageProtocolReader to IBinaryXmppProtocolConverter.

--- oxm/src/main/java/com/thefirstlineofcode/basalt/oxm/binary/IBinaryXmppProtocolConverter.java
+++ oxm/src/main/java/com/thefirstlineofcode/basalt/oxm/binary/IBinaryXmppProtocolConverter.java
@@ -1,6 +1,9 @@
 package com.thefirstlineofcode.basalt.oxm.binary;
 
+import com.thefirstlineofcode.basalt.protocol.core.Protocol;
+
 public interface IBinaryXmppProtocolConverter {
 	byte[] toBinary(String message);
 	String toXml(byte[] message);
+	Protocol readProtocol(byte[] data);
 }
