408d421c5361e2d238b58bd2453575e9f0de21a2
Jelly Bean <jelly_b@sina.com>
2022-04-10 22:13:46 +0800

    Refactor to move constant values to Constants class.

--- oxm/src/main/java/com/thefirstlineofcode/basalt/oxm/binary/BinaryUtils.java
+++ oxm/src/main/java/com/thefirstlineofcode/basalt/oxm/binary/BinaryUtils.java
@@ -5,15 +5,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class BinaryUtils {
-	private static final byte FLAG_DOC_BEGINNING_END = (byte) 0xff;
-	private static final byte FLAG_UNIT_SPLLITER = (byte) 0xfe;
-	private static final byte FLAG_ESCAPE = (byte)0xfd;
-	private static final byte FLAG_NOREPLACE = (byte)0xfc;
-	
-	private static final String PREFIX_STRING_BASE64_ENCODED = "$B{";
-	private static final String POSTFIX_STRING_OF_BASE64_ENCODED = "}";
-	
-	private static final String PREFIX_HEX_NUMBER = "0x";
+
 	
 	public static String getHexStringFromBytes(byte[] bytes) {
 		StringBuilder sb = new StringBuilder();
@@ -30,16 +22,16 @@ public class BinaryUtils {
 	}
 	
 	public static byte[] getBytesFromHexString(String hexString) {
-		if (!hexString.startsWith(PREFIX_HEX_NUMBER)) {
+		if (!hexString.startsWith(Constants.PREFIX_HEX_NUMBER)) {
 			throw new IllegalArgumentException("Invalid HEX string: " + hexString);
 		}
 		
-		String[] bytesString = hexString.split(PREFIX_HEX_NUMBER);
+		String[] bytesString = hexString.split(Constants.PREFIX_HEX_NUMBER);
 		byte[] bytes = new byte[bytesString.length - 1];
 		for (int i = 1; i < bytesString.length; i++) {
 			int num;
 			try {
-				num = Integer.decode(PREFIX_HEX_NUMBER + bytesString[i]);
+				num = Integer.decode(Constants.PREFIX_HEX_NUMBER + bytesString[i]);
 			} catch (NumberFormatException e) {
 				throw new IllegalArgumentException("Invalid HEX string: " + hexString);
 			}
@@ -61,41 +53,41 @@ public class BinaryUtils {
 		
 		data = Arrays.copyOfRange(data, 2, data.length);
 		return String.format("%s%s%s",
-				PREFIX_STRING_BASE64_ENCODED,
+				Constants.PREFIX_STRING_BASE64_ENCODED,
 				Base64.encodeToString(data, false),
-				POSTFIX_STRING_OF_BASE64_ENCODED
+				Constants.POSTFIX_STRING_OF_BASE64_ENCODED
 			);
 	}
 	
 	public static boolean isBase64Encoded(String content) {
 		return content.length() > 4 &&
-				content.startsWith(PREFIX_STRING_BASE64_ENCODED) &&
-				content.endsWith(POSTFIX_STRING_OF_BASE64_ENCODED);
+				content.startsWith(Constants.PREFIX_STRING_BASE64_ENCODED) &&
+				content.endsWith(Constants.POSTFIX_STRING_OF_BASE64_ENCODED);
 	}
 	
 	public static boolean isBase64Decoded(byte[] bytes) {
 		return bytes.length > 2 &&
-				bytes[0] == FLAG_NOREPLACE &&
-				bytes[1] == FLAG_NOREPLACE;
+				bytes[0] == Constants.FLAG_NOREPLACE &&
+				bytes[1] == Constants.FLAG_NOREPLACE;
 	}
 	
 	public static byte[] escape(byte[] bytes) {
 		if (bytes.length == 1) {
-			return new byte[] {FLAG_NOREPLACE, bytes[0]};
+			return new byte[] {Constants.FLAG_NOREPLACE, bytes[0]};
 		}
 		
 		if (bytes.length == 2) {
-			return new byte[] {FLAG_NOREPLACE, bytes[0], bytes[1]};
+			return new byte[] {Constants.FLAG_NOREPLACE, bytes[0], bytes[1]};
 		}
 		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		try {
 			for (int i = 0; i < bytes.length; i++) {
-				if (bytes[i] == FLAG_DOC_BEGINNING_END ||
-						bytes[i] == FLAG_UNIT_SPLLITER ||
-						bytes[i] == FLAG_NOREPLACE ||
-						bytes[i] == FLAG_ESCAPE) {
-					output.write(FLAG_ESCAPE);
+				if (bytes[i] == Constants.FLAG_DOC_BEGINNING_END ||
+						bytes[i] == Constants.FLAG_UNIT_SPLLITER ||
+						bytes[i] == Constants.FLAG_NOREPLACE ||
+						bytes[i] == Constants.FLAG_ESCAPE) {
+					output.write(Constants.FLAG_ESCAPE);
 					output.write(bytes[i]);
 				} else {
 					output.write(bytes[i]);
@@ -114,18 +106,18 @@ public class BinaryUtils {
 	}
 	
 	public static byte[] unescape(byte[] bytes) throws IOException {
-		if (bytes.length == 3 && bytes[0] == FLAG_ESCAPE) {
+		if (bytes.length == 3 && bytes[0] == Constants.FLAG_ESCAPE) {
 			return new byte[] {bytes[1], bytes[2]};
 		}
 		
-		if (bytes.length == 2 && bytes[0] == FLAG_ESCAPE) {
+		if (bytes.length == 2 && bytes[0] == Constants.FLAG_ESCAPE) {
 			return new byte[] {bytes[1]};
 		}
 		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		try {
 			for (int i = 0; i < bytes.length; i++) {
-				if (bytes[i] == FLAG_ESCAPE && i < bytes.length && isEscapedByte(bytes[i + 1])) {
+				if (bytes[i] == Constants.FLAG_ESCAPE && i < bytes.length && isEscapedByte(bytes[i + 1])) {
 					continue;
 				}
 				
@@ -140,10 +132,10 @@ public class BinaryUtils {
 	}
 	
 	public static boolean isEscapedByte(byte b) {
-		return b == FLAG_DOC_BEGINNING_END ||
-				b == FLAG_UNIT_SPLLITER ||
-				b== FLAG_NOREPLACE||
-				b == FLAG_ESCAPE;
+		return b == Constants.FLAG_DOC_BEGINNING_END ||
+				b == Constants.FLAG_UNIT_SPLLITER ||
+				b== Constants.FLAG_NOREPLACE||
+				b == Constants.FLAG_ESCAPE;
 	}
 	
 	public static byte[] decodeFromBase64(String content) {
@@ -154,8 +146,8 @@ public class BinaryUtils {
 		byte[] decodedPart = BinaryUtils.escape(Base64.decode(encodedPart));
 		
 		byte[] withDecodeFlag = new byte[decodedPart.length + 2];
-		withDecodeFlag[0] = FLAG_NOREPLACE;
-		withDecodeFlag[1] = FLAG_NOREPLACE;
+		withDecodeFlag[0] = Constants.FLAG_NOREPLACE;
+		withDecodeFlag[1] = Constants.FLAG_NOREPLACE;
 		for (int i = 0; i < decodedPart.length; i++) {
 			withDecodeFlag[i + 2] = decodedPart[i];
 		}
@@ -165,13 +157,13 @@ public class BinaryUtils {
 	
 	public static byte[] getBytesWithBase64DecodedFlag(byte[] bytes) {
 		if (bytes.length > 2 &&
-				bytes[0] == FLAG_NOREPLACE &&
-				bytes[1] == FLAG_NOREPLACE)
+				bytes[0] == Constants.FLAG_NOREPLACE &&
+				bytes[1] == Constants.FLAG_NOREPLACE)
 			return bytes;
 		
 		byte[] withBase64DecodedFlag = new byte[bytes.length + 2];
-		withBase64DecodedFlag[0] = FLAG_NOREPLACE;
-		withBase64DecodedFlag[1] = FLAG_NOREPLACE;
+		withBase64DecodedFlag[0] = Constants.FLAG_NOREPLACE;
+		withBase64DecodedFlag[1] = Constants.FLAG_NOREPLACE;
 		
 		for (int i = 0; i < bytes.length; i++) {
 			withBase64DecodedFlag[i + 2] = bytes[i];
