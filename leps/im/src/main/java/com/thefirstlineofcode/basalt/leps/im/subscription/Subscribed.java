d37d25bce93daa5b42978c814a5290bb0c49ddb4
Jelly Bean <jelly_b@sina.com>
2022-05-29 00:15:43 +0800

    Remove LEPs IM protocol project.

--- leps/im/src/main/java/com/thefirstlineofcode/basalt/leps/im/subscription/Subscribed.java
+++
@@ -1,9 +0,0 @@
-package com.thefirstlineofcode.basalt.leps.im.subscription;
-
-import com.thefirstlineofcode.basalt.oxm.convention.annotations.ProtocolObject;
-import com.thefirstlineofcode.basalt.protocol.core.Protocol;
-
-@ProtocolObject(namespace="urn:leps:subscription", localName="subscribed")
-public class Subscribed {
-	public static final Protocol PROTOCOL = new Protocol("urn:leps:subscription", "subscribed");
-}
